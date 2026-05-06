4d8b9da49339772d190f1723379dd0a360a83856
fina-w <safietoudiangarecfpt@gmail.com>
2026-05-06 09:42:14 +0000

    widget_test.dart corrected

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:galsen_telecom/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GalsenTelecomApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
